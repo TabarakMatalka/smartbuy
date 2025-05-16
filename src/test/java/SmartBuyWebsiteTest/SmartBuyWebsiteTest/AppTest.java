8c7ae71fdfd7adf81d9978f8a2783fc10e87f5d8
TabarakMatalka <147642795+TabarakMatalka@users.noreply.github.com>
2025-05-17 01:17:23 +0300

    add database methods

--- src/test/java/SmartBuyWebsiteTest/SmartBuyWebsiteTest/AppTest.java
+++ src/test/java/SmartBuyWebsiteTest/SmartBuyWebsiteTest/AppTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -29,8 +30,52 @@ public class AppTest {
 			driver.get(URL);
 			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels","root","1234");
 		}
+		@Test(priority=1, enabled=false)
+		public void InsertIntoDatabase() throws SQLException {
+			String query="INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, city, country, salesRepEmployeeNumber, creditLimit) VALUES (123, 'New Corp', 'Smith', 'John', '123456789', '123 Main St', 'Los Angeles', 'USA', 1370, 50000.00);";
+			
+			statment=connection.createStatement();
+			int rowEffected=statment.executeUpdate(query);
+			System.out.println(rowEffected);
+		}
 		
-		@Test (priority=1)
+		@Test(priority=2,enabled=false)
+		public void UpdateDatabase() throws SQLException {
+			String query="UPDATE customers SET creditLimit = 76000 WHERE customerNumber = 999;";
+			
+			statment=connection.createStatement();
+			int rowEffected=statment.executeUpdate(query);
+			System.out.println(rowEffected);
+		}
+		
+		@Test(priority=3)
+		public void ReadDatabase() throws SQLException {
+			String query="SELECT * FROM customers WHERE customerNumber = 999;";
+			
+			statment=connection.createStatement();
+			resultSet=statment.executeQuery(query);
+			while(resultSet.next()) {
+			   String customerName= resultSet.getNString("customerName");
+			   System.out.println(customerName);
+			}
+		}
+		
+		@Test (priority=4)
+		public void DeleteProductDatabase() throws SQLException {
+			String query="DELETE FROM customers WHERE  customerNumber = 999;";
+			
+			statment=connection.createStatement();
+			int rowEffected=statment.executeUpdate(query);
+			System.out.println(rowEffected);
+			if(rowEffected==1) {
+			   System.out.println("customer deleted");
+			}
+			else {
+				 System.out.println("customer already deleted or not exists ");
+			}
+			
+		}
+		@Test (priority=5)
 		public void RegisterForm() throws SQLException {
 			String query="SELECT * FROM customers WHERE customerNumber = 406;";
 			statment=connection.createStatement();
@@ -59,5 +104,9 @@ public class AppTest {
             WebElement passwordInput=driver.findElement(By.id("customer[password]"));
             passwordInput.sendKeys(customerPassword);
             
+		}
+		@AfterTest
+		public void tearDown() {
+			driver.quit(); 
 		}
 }
